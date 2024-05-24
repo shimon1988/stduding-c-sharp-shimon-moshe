a28e3a4590fddd4abc6b536f488174078f0de54f
Shimon Moshe <shimon1988@gmail.com>
2024-05-24 11:24:49 +0300

    Add project files.

---
+++ stduding c-sharp shimon moshe/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stduding_c_sharp_shimon_moshe
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
